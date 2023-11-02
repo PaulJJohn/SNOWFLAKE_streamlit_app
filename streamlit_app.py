ac1c76fb90dc702cd96011ff35a8df62692dbd65
Paul J John <82320324+PaulJJohn@users.noreply.github.com>
2023-11-01 20:44:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,10 +29,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  #Date just spilled to screen.
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# X - streamlit.text(fruityvice_response.json())  #Data just spilled to screen.
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# Display Data int a Grid.
 #streamlit.dataframe(fruityvice_normalized)
