9fc1f83ca09090b0406846034b7d7a1b81667c05
Paul J John <82320324+PaulJJohn@users.noreply.github.com>
2023-11-04 18:29:01 -0400

    Update streamlit_app.py
    Unbelievable, Number of tabs makes a diffenece and causes errors if not correct!

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice API request.
+# Unbelievable, Number of tabs makes a diffenece and causes errors if not correct!
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
