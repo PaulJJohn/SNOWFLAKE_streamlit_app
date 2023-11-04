c427b6749fec2b2dfec6c39acd0b548ac6971a80
Paul J John <82320324+PaulJJohn@users.noreply.github.com>
2023-11-04 17:44:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,12 +30,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 # Display Data in a Grid.
-streamlit.dataframe(fruit_choice)
+#streamlit.dataframe(fruit_choice)
 
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())  #Data just spilled to screen.
 
 # write your own comment -what does the next line do? 
