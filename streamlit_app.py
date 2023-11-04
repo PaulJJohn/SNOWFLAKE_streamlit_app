11b734bcdf6861164590011a534a8125a07c1777
Paul J John <82320324+PaulJJohn@users.noreply.github.com>
2023-11-04 19:04:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,6 @@ def get_fruityvice_data(this_fruit_choice):
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       return fruityvice_normalized
 
-
 #New section to display fruityvice API request.
 # Unbelievable, Number of tabs makes a diffenece and causes errors if not correct!
 streamlit.header('Fruityvice Fruit Advice!')
@@ -64,17 +63,27 @@ except URLError as e:
 # Display Data in a Grid.
 # streamlit.dataframe(fruityvice_normalized)
 
+# import snowflake.connector
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+
+streamlit.header("Fruit Load List:")
+#Snowflake-related functions
+def get_fruit_load_list():
+      with my_cnx.cursor() as my_cur:
+            my_cur.execute("select * from fruit_load_list")
+            return my_cur.fetchall()
+
+# Add a button to load the fruit      
+if streamlit.button('Get Fruit Load List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
+
 # Don't run anything past here while we trobleshoot
 streamlit.stop()
-
-# import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("Fruit Load List:")
-streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
